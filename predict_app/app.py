1954878ff2024ab27ff1737ffc32781ff85616fa
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-11-30 21:24:35 -0500

    headers fixed

--- predict_app/app.py
+++ predict_app/app.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
+
+# Load model from trained session and set up classes
 @st.cache
 def fetch_model():
     return tf.keras.models.load_model('models/cifar_cnn1.h5')
@@ -12,6 +14,7 @@ loaded_model = fetch_model()
 class_names = ['airplane', 'car', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
+# Define a function to process an image and output a category in class_names
 def pred_img(x):
     img1 = Image.open(x).convert(mode="RGB")
     img1 = img1.resize((32,32))
@@ -19,6 +22,7 @@ def pred_img(x):
     img_np_array = np.reshape(array1, (32,32,3)) / 255.0
     return class_names[np.argmax(loaded_model.predict(np.expand_dims(img_np_array, axis=0)))]
 
+# Define the user interface
 def app():
     st.title('CIFAR Image Classification')
     st.write('Use a CNN to classify images into general categories')
