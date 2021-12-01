aa10a5175f3cd758f91f372b786b3f60c872939b
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-11-30 21:22:50 -0500

    prediction app built

--- predict_app/app.py
+++ predict_app/app.py
@@ -1,1 +1,41 @@
 import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+@st.cache
+def fetch_model():
+    return tf.keras.models.load_model('models/cifar_cnn1.h5')
+
+loaded_model = fetch_model()
+
+class_names = ['airplane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+def pred_img(x):
+    img1 = Image.open(x).convert(mode="RGB")
+    img1 = img1.resize((32,32))
+    array1 = np.array(img1.getdata())
+    img_np_array = np.reshape(array1, (32,32,3)) / 255.0
+    return class_names[np.argmax(loaded_model.predict(np.expand_dims(img_np_array, axis=0)))]
+
+def app():
+    st.title('CIFAR Image Classification')
+    st.write('Use a CNN to classify images into general categories')
+    st.markdown('----')
+
+    col1, col2 = st.beta_columns([2,1])
+
+    with col1:
+        raw_image = st.file_uploader('Upload an Image')
+
+        if raw_image:
+            st.image(raw_image)
+
+    with col2:
+        st.write('Make a Prediction')
+        if st.button('Run Model') and raw_image:
+            st.write(f'`Prediction: ` {pred_img(raw_image)}')
+
+if __name__ =='__main__':
+    app()
