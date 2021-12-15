77aec7a7763fa2481675b9cb353225285214dfa2
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-12-14 23:06:38 -0500

    gpu

--- predict_app/model.py
+++ predict_app/model.py
@@ -2,3 +2,6 @@ import tensorflow as tf
 
 def fetch_model():
     return tf.keras.models.load_model('models/cifar_cnn1.h5')
+
+def gpu():
+    tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
