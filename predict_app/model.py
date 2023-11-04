683d5b204b4ea81f0493a5cc2512de9c5c3e9344
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-11-03 23:45:24 -0400

    CHECK gpu

--- predict_app/model.py
+++ predict_app/model.py
@@ -4,4 +4,5 @@ def fetch_model():
     return tf.keras.models.load_model('models/cifar_cnn1.h5')
 
 def check_gpu():
-    tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
+    tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None) 
+    
